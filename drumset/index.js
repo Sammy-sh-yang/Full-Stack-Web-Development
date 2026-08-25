ab4dfeaf1e7dbc8235ec6903a3bff03a986bc9dc
Sammy-sh-yang <95999650+Sammy-sh-yang@users.noreply.github.com>
2026-08-25 23:49:24 +0000

    Update drumset files

---
+++ drumset/index.js
@@ -0,0 +1,27 @@
+
+var drumButtons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener("click", function() {
+
+        switch (this.innerText) {
+    case 'w':
+        alert("this is w");
+        break;
+    case 'a':
+        alert("this is a");
+        break;
+    case 's':
+        alert("this is s");
+        break;
+    case 'd':
+        alert("this is d");
+        break;
+    default:
+        break;
+}
+
+    });
+}
+
+
