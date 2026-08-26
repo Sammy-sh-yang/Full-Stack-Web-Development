734890f10a2d671a0efe4ac3e6db604580af2cdf
Sammy-sh-yang <95999650+Sammy-sh-yang@users.noreply.github.com>
2026-08-26 18:24:22 +0000

    Add debugger statement to drum button click event and create launch configuration for VSCode

--- drumset challange/index.js
+++ drumset challange/index.js
@@ -3,9 +3,9 @@ var drumButtons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function() {
-
+    debugger;
         switch (this.innerText) {
-    case 'w':
+            case 'w':
         alert("this is w");
         break;
     case 'a':
