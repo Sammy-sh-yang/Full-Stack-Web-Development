785a7f5e64280bf39d7cc4f2cb68a04a31279612
Sammy-sh-yang <95999650+Sammy-sh-yang@users.noreply.github.com>
2026-08-28 01:11:16 +0000

    Add countChar and sortArray functions for character counting and array sorting

---
+++ JavaScript Coding Questions/sortArray.js
@@ -0,0 +1,19 @@
+
+// not completed
+
+var array1 = [5, 3, 8, 1]; 
+
+
+function sortArray (ranArray){
+
+    var blank = []; 
+    for (var i = 0; i < ranArray.length; i++){
+        for (var j = 1; j < blank.length; j++){
+            if (blank[i] < blank[j]){
+
+            }
+        }
+
+    }
+
+}
